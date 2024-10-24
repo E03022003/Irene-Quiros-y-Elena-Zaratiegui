7c5d9f6a385a5dea12d92a835971c50444331c6c
Elena Zaratiegui <ezaratieguigarcia@gmail.com>
2024-10-24 23:21:31 +0200

    edit cards

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    // buscador intento de oculto y que a clic aparezca
-    $('#search-btn').click(function() {
-        $('#search-form').toggle();
+    // Mostrar/ocultar el formulario de búsqueda al hacer clic en la lupa
+    $('#search-btn').click(function(event) {
+        event.stopPropagation(); // Evitar que el clic en el botón cierre inmediatamente el formulario
+        $('#search-form').toggle(); // Alterna la visibilidad del formulario
+    });
+
+    // Ocultar el formulario de búsqueda al hacer clic en cualquier parte fuera de él
+    $(document).click(function(event) {
+        if (!$(event.target).closest('#search-form, #search-btn').length) {
+            $('#search-form').hide(); // Si el clic es fuera del formulario o el botón, ocúltalo
+        }
+    });
+
+    // Evitar que los clics dentro del formulario cierren el mismo
+    $('#search-form').click(function(event) {
+        event.stopPropagation();
     });
 });
