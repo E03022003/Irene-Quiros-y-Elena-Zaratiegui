edbb13c56bfd8e98fea77b2c893356b3883e114d
Elena Zaratiegui <ezaratieguigarcia@gmail.com>
2024-11-10 17:23:47 +0100

    navbar

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
 // NAVBAR
-$(document).ready(function () {
+/* $(document).ready(function () {
     // Al hacer clic en el icono de búsqueda
     $('#search-icon').on('click', function (event) {
         event.stopPropagation();
         const searchInput = $('#search-input');
 
+        // Alterna la visibilidad del input
+        searchInput.toggleClass('active');
         if (searchInput.hasClass('active')) {
-            searchInput.removeClass('active').css({ 'display': 'none', 'width': '0' });
-        } else {
-            searchInput.addClass('active').css({ 'display': 'block', 'width': '200px' });
-            searchInput.focus(); 
+            searchInput.focus();
         }
     });
 
     // Cierra el campo de búsqueda al hacer clic fuera de él
     $(document).on('click', function (event) {
         if (!$(event.target).closest('#search-form').length) {
-            $('#search-input').removeClass('active').css({ 'display': 'none', 'width': '0' });
+            $('#search-input').removeClass('active');
         }
     });
+}); */
+
+$(document).ready(function() {
+    // Mostrar/ocultar barra de búsqueda al hacer clic en el icono de lupa
+    $('#search-icon').on('click', function() {
+        $('.search-bar').toggleClass('active');
+    });
 });
+
+
+
+
+
 
 // BURBUJA PARA BAJAR Y SUBIR
 $(document).ready(function() {
